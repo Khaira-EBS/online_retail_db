d138b6943a64d113ed0e87213520ffdfd6d5d477
Khaira Isyara <khairaisyara25@gmail.com>
2023-11-11 20:25:33 +0700

    insert data customer

---
+++ insertdatatable.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer (name_customer, email_customer, address_customer, phone_customer)
+VALUES ('Khaira Isyara', 'khairaisyara25@gmail.com', 'Bekasi Jatben', '+6212345678'),
+('Khaira Isyara', 'khairaisyara25@gmail.com', 'Bekasi Jatben', '+6212345678'),
+('Khaira Isyara', 'khairaisyara25@gmail.com', 'Bekasi Jatben', '+6212345678'),
+('Khaira Isyara', 'khairaisyara25@gmail.com', 'Bekasi Jatben', '+6212345678');
